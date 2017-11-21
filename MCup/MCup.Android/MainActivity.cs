3688eba366492af1639349a6af4a4722c7ce2c58
Antonio Valente <antosaverio.valente@gmail.com>
2017-11-21 15:14:02 +0100

    ho modificato lo splash creen android

--- MCup/MCup.Android/MainActivity.cs
+++ MCup/MCup.Android/MainActivity.cs
@@ -1,11 +1,11 @@
 ﻿using System;
-
 using Android.App;
 using Android.Content.PM;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using static MCup.Droid.MainActivity;
 
 namespace MCup.Droid
 {
--- MCup/MCup.Android/SplashScreen.cs
+++ MCup/MCup.Android/SplashScreen.cs
@@ -1,13 +1,17 @@
 ﻿using Android.App;
 using Android.OS;
 using Android.Content.PM;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using MCup.Droid;
 
 namespace QuizAmoroso.Droid
 {
     [Activity(Label = "MCup", Icon = "@drawable/icon", Theme = "@style/SplashTheme", MainLauncher = true, NoHistory = true, ScreenOrientation = ScreenOrientation.Portrait, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class SplashScreen : Activity
     {
-        protected override void OnCreate(Bundle bundle)
+        protected async override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
             await Task.Delay(1000);
