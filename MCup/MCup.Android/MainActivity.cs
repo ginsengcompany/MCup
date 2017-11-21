17b6d4ff004a064897eff176fe1ec57a4c205ee1
Antonio Valente <antosaverio.valente@gmail.com>
2017-11-21 14:39:20 +0100

    aggiungo modifiche per attivare lo splash screen in android

--- MCup/MCup.Android/MainActivity.cs
+++ MCup/MCup.Android/MainActivity.cs
@@ -9,7 +9,7 @@ using Android.OS;
 
 namespace MCup.Droid
 {
-    [Activity(Label = "MCup", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "MCup", Icon = "@drawable/icon", Theme = "@style/MainTheme", ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle bundle)
---
+++ MCup/MCup.Android/SplashScreen.cs
@@ -0,0 +1,17 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Content.PM;
+
+namespace QuizAmoroso.Droid
+{
+    [Activity(Label = "MCup", Icon = "@drawable/icon", Theme = "@style/SplashTheme", MainLauncher = true, NoHistory = true, ScreenOrientation = ScreenOrientation.Portrait, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class SplashScreen : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            await Task.Delay(1000);
+            StartActivity(typeof(MainActivity));
+        }
+    }
+}
