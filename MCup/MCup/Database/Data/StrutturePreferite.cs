3d42745b84601f769e33846fdb6b07931e597987
Antonio Valente <antosaverio.valente@gmail.com>
2017-11-23 12:12:50 +0100

    aggiungo classe StruttuePreferite per SQLite

---
+++ MCup/MCup/Database/Data/StrutturePreferite.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using MCup.Database.Models;
+
+namespace MCup.Database.Data
+{
+    public static class StrutturePreferite
+    {
+        public static int GetCountStrutturePreferite()
+        {
+            int count = Database.Connection.ExecuteScalar<int>("SELECT COUNT(*) FROM TbStrutturePreferite");
+            return count;
+        }
+
+        public static void InserisciStrutturaPreferita(TbStrutturePreferite struttura)
+        {
+            Database.Connection.Insert(struttura);
+        }
+
+        public static List<TbStrutturePreferite> VisualizzaStrutturePreferite()
+        {
+            return Database.Connection.Query<TbStrutturePreferite>("SELECT * FROM TbStrutturePreferite");
+        }
+
+        public static List<TbStrutturePreferite> PrelevaIdStruttura(string id)
+        {
+            return Database.Connection.Query<TbStrutturePreferite>("SELECT * FROM TbStrutturePreferite WHERE City = ?", id);
+        }
+
+        public static IEnumerable<TbStrutturePreferite> UpdateStrutturaPreferita(string nomeStruttura, string DscrizioneStruttura, int id)
+        {
+            return Database.Connection.Query<TbStrutturePreferite>("UPDATE TbStrutturePreferite SET NomeStruttura = ?, DescrizioneStruttura = ? WHERE id = ?; ", nomeStruttura, DscrizioneStruttura, id);
+        }
+    }
+}
+
+
+
+
---
+++ MCup/MCup/Database/Models/TbStrutturePreferite.cs
@@ -0,0 +1,21 @@
+﻿using SQLite;
+
+namespace MCup.Database.Models
+{
+    [Table("TbStrutturePreferite")]
+    public class TbStrutturePreferite
+    {
+        [PrimaryKey, NotNull, AutoIncrement, Column("id")]
+        public int id { get; set; }
+
+        [NotNull]
+        public string NomeStruttura { get; set; }
+
+        [NotNull]
+        public string DescrizioneStruttura { get; set; }
+
+        public TbStrutturePreferite(int _id) { }
+
+        public TbStrutturePreferite() { }
+    }
+}
