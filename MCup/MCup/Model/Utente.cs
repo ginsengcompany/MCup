f37e7fb562128707e620f6f9b27bd7bf6e1702bb
aldo <aldo.minasi@gmail.com>
2017-12-20 09:51:25 +0100

    ho modificato la classe Utente

--- MCup/MCup/Model/Utente.cs
+++ MCup/MCup/Model/Utente.cs
@@ -3,8 +3,33 @@ namespace MCup.Model
 {
     public class Utente
     {
+        public string nome { get; set; }
+        public string cognome { get; set; }
+        public string codice_fiscale { get; set; }
+        public string password { get; set; }
+
         public Utente()
         {
+            nome = "";
+            cognome = "";
+            codice_fiscale = "";
+            password = "";
+        }
+
+        public Utente(Utente utente)
+        {
+            this.nome = utente.nome;
+            this.cognome = utente.cognome;
+            this.codice_fiscale = utente.codice_fiscale;
+            this.password = utente.password;
+        }
+
+        public Utente(string nome, string cognome, string codice_fiscale, string password)
+        {
+            this.nome = nome;
+            this.cognome = cognome;
+            this.codice_fiscale = codice_fiscale;
+            this.password = password;
         }
     }
 }
