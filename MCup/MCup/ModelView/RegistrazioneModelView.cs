87b34bb9c04204808046c316fb277b9f6a681f07
aldo <aldo.minasi@gmail.com>
2017-12-20 10:55:25 +0100

    ho creato i file per la registrazione

---
+++ MCup/MCup/ModelView/RegistrazioneModelView.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MCup.Model;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace MCup.ModelView
+{
+    public class RegistrazioneModelView : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private Utente utente;
+
+        public string codiceFiscale
+        {
+            get { return utente.codice_fiscale; }
+            set
+            {
+                OnPropertyChanged();
+                utente.codice_fiscale = value;
+            }
+        }
+
+        public string password
+        {
+            get { return utente.password; }
+            set
+            {
+                OnPropertyChanged();
+                utente.password = value;
+            }
+        }
+
+        public string nome
+        {
+            get { return utente.nome; }
+            set
+            {
+                OnPropertyChanged();
+                utente.nome = value;
+            }
+        }
+
+        public string cognome
+        {
+            get { return utente.cognome; }
+            set
+            {
+                OnPropertyChanged();
+                utente.cognome = value;
+            }
+        }
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string name = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
+        public RegistrazioneModelView()
+        {
+            utente = new Utente();
+        }
+    }
+}
---
+++ MCup/MCup/Views/Registrazione.xaml.cs
@@ -0,0 +1,22 @@
+﻿using MCup.ModelView;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MCup.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Registrazione : ContentPage
+    {
+        public Registrazione()
+        {
+            InitializeComponent();
+            BindingContext = new RegistrazioneModelView();
+        }
+    }
+}
