ff38a1db1034811f3ad7159a55dbef86bd9201ae
FabriGinseng <antoniofabriziofiume@libero.it>
2017-12-22 12:36:11 +0100

    aggiunta riga

--- MCup/MCup.iOS/AppDelegate.cs
+++ MCup/MCup.iOS/AppDelegate.cs
@@ -5,6 +5,7 @@ using System.Linq;
 
 using Foundation;
 using UIKit;
+using Xfx;
 
 namespace MCup.iOS
 {
@@ -23,6 +24,7 @@ namespace MCup.iOS
         //
         public override bool FinishedLaunching(UIApplication app, NSDictionary options)
         {
+            XfxControls.Init();
             global::Xamarin.Forms.Forms.Init();
             LoadApplication(new App());
 
