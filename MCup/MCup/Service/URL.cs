57d95aa6de5371c369fc3f9531b51de954378d61
aldo <aldo.minasi@gmail.com>
2017-12-28 11:58:28 +0100

    cambiati url

--- MCup/MCup/Service/URL.cs
+++ MCup/MCup/Service/URL.cs
@@ -8,24 +8,24 @@ namespace MCup.Service
 {
     public static class URL
     {
-        public static string Calendario = "http://192.168.125.69:3000/calendario";
-        public static string Strutture = "http://192.168.125.69:3000/strutture";
-        public static string TerminiServizio = "http://192.168.125.69:3000/terminiservizio";
-        public static string Ricetta = "http://192.168.125.69:3000/ricetta";
-        public static string StruttureErogatrici = "http://192.168.125.69:3000/controlloPrestazioni";
-        public static string Registrazione = "http://192.168.125.69:3000/auth/registrazione";
-        public static string Login = "http://192.168.125.69:3000/auth/login";
-        public static string StrutturaPreferita = "http://192.168.125.69:3000/auth/strutturaPreferita";
+        //public static string Calendario = "http://192.168.125.69:3000/calendario";
+        //public static string Strutture = "http://192.168.125.69:3000/strutture";
+        //public static string TerminiServizio = "http://192.168.125.69:3000/terminiservizio";
+        //public static string Ricetta = "http://192.168.125.69:3000/ricetta";
+        //public static string StruttureErogatrici = "http://192.168.125.69:3000/controlloPrestazioni";
+        //public static string Registrazione = "http://192.168.125.69:3000/auth/registrazione";
+        //public static string Login = "http://192.168.125.69:3000/auth/login";
+        //public static string StrutturaPreferita = "http://192.168.125.69:3000/auth/strutturaPreferita";
 
 
-        //public static string Calendario = "http://192.168.125.14:3000/calendario";
-        //public static string Strutture = "http://192.168.125.14:3000/strutture";
-        //public static string TerminiServizio = "http://192.168.125.14:3000/terminiservizio";
-        //public static string Ricetta = "http://192.168.125.14:3000/ricetta";
-        //public static string StruttureErogatrici = "http://192.168.125.14:3000/controlloPrestazioni";
-        //public static string Registrazione = "http://192.168.125.14:3000/auth/registrazione";
-        //public static string Login = "http://192.168.125.14:3000/auth/login";
-        //public static string StrutturaPreferita = "http://192.168.125.14:3000/auth/strutturaPreferita";
+        public static string Calendario = "http://192.168.125.14:3000/calendario";
+        public static string Strutture = "http://192.168.125.14:3000/strutture";
+        public static string TerminiServizio = "http://192.168.125.14:3000/terminiservizio";
+        public static string Ricetta = "http://192.168.125.14:3000/ricetta";
+        public static string StruttureErogatrici = "http://192.168.125.14:3000/controlloPrestazioni";
+        public static string Registrazione = "http://192.168.125.14:3000/auth/registrazione";
+        public static string Login = "http://192.168.125.14:3000/auth/login";
+        public static string StrutturaPreferita = "http://192.168.125.14:3000/auth/strutturaPreferita";
 
     }
 }
