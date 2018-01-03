951e5565e13cb50cded4788e0341ce47e61bfa45
aldo <aldo.minasi@gmail.com>
2018-01-03 11:25:34 +0100

    ho aggiunto una nuova pagina per inserire un nuovo contatto nella lista dei contatti

---
+++ MCup/MCup/Views/NuovoContatto.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MCup.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class NuovoContatto : ContentPage
+    {
+        public NuovoContatto()
+        {
+            InitializeComponent();
+        }
+    }
+}
