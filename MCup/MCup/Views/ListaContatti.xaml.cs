e3fcda6413afc832cb36b8091eda83c346f48037
FabriGinseng <antoniofabriziofiume@libero.it>
2018-01-03 15:19:16 +0100

    listview lista contatti

--- MCup/MCup/Views/ListaContatti.xaml.cs
+++ MCup/MCup/Views/ListaContatti.xaml.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using MCup.ModelView;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -15,7 +15,8 @@ namespace MCup.Views
         public ListaContatti()
         {
             InitializeComponent();
-            
+            BindingContext = new ListaContattiModelView();
+
         }
        
 }
