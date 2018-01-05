41038395f61cd3a4f8d00eca673b114a8fa3b372
AlessioRivoli <pleyboy87@hotmail.it>
2018-01-05 11:03:20 +0100

    aggiunto infoContatto

---
+++ MCup/MCup/Views/InfoContatto.xaml.cs
@@ -0,0 +1,22 @@
+﻿using MCup.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MCup.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class InfoContatto : ContentPage
+    {
+        public InfoContatto(Contatto infoContatto)
+        {
+            InitializeComponent();
+
+        }
+    }
+}
