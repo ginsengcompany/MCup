54a6ae453b34106b0e7e3d558b7ca88f05faa6be
aldo <aldo.minasi@gmail.com>
2018-01-08 14:43:28 +0100

    ho inserito i controlli in formPrenotazione e ho aggiunto alla classe utenzaPrenotazione il metodo checkCodiceFiscale che restituisce un valore booleano in base alla correttezza del codice fiscale dell'oggetto

--- MCup/MCup/Model/UtenzaPrenotazione.cs
+++ MCup/MCup/Model/UtenzaPrenotazione.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace MCup.Model
@@ -12,7 +13,7 @@ namespace MCup.Model
         public string nome { get; set; } //nome dell'utente
         public string cognome { get; set; } //cognome dell'utente
         private string codice_fiscale { get; set; } //codice fiscale dell'utente
-
+        private Regex regexCodFisc = new Regex(@"^[A-Za-z]{6}[0-9]{2}[A-Za-z]{1}[0-9]{2}[A-Za-z]{1}[0-9]{3}[A-Za-z]{1}$");//Espressione regolare utilizzata per controllare che il codice fiscale sia corretto
         //Costruttore con parametri
         public UtenzaPrenotazione(string name, string surname, string cod)
         {
@@ -34,6 +35,11 @@ namespace MCup.Model
         public void setCodiceFiscale(string codicefiscale)
         {
             this.codice_fiscale = codicefiscale;
+        }
+
+        public bool checkCodiceFiscale()
+        {
+            return regexCodFisc.IsMatch(this.codice_fiscale);
         }
     }
 }
