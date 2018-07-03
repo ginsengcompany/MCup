ca883a8644dc978ac7303d80aa4db3d35f02818f
aldominasi <aldo.minasi@gmail.com>
2018-07-03 16:19:40 +0200

    ho implementato il picker con la label

--- MCup/MCup/Model/PickerProperties.cs
+++ MCup/MCup/Model/PickerProperties.cs
@@ -3,12 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
 
 namespace MCup.Model
 {
-   public class PickerProperties
-   {
-       public string title { get; set; }
+    public class PickerProperties
+    {
+        public string title { get; set; }
         public bool enabled { get; set; }
+        public ICommand SetFocusPicker {
+            get
+            {
+                return new Command<Picker>((Picker picker) =>
+                {
+                    if(enabled)
+                        picker.Focus();
+                });
+            }
+        }
     }
 }
